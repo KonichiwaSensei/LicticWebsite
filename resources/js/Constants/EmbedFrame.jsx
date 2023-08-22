b05603141aedbd79945166907cc56cade8009eb0
Matthew Shenar <matthew.shenar@gmail.com>
2023-08-22 15:57:44 +0200

    more tweaks to reactive design

--- resources/js/Constants/EmbedFrame.jsx
+++ resources/js/Constants/EmbedFrame.jsx
@@ -24,7 +24,7 @@ export default function EmbedFrame({pText, pClassName, iframeClassName, url}) {
                 className={`${iframeClassName} ${shakingAnimation}`}
                 src={url}
                 title="Lictic Embed"
-                // frameBorder="1"
+                frameBorder="1"
                 // allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
                 loading="lazy"
                 allowFullScreen
