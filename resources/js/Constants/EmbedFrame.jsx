d15af69f98d3b735a47026853048a057744db132
Matthew Shenar <matthew.shenar@gmail.com>
2023-08-22 12:34:56 +0200

    iframe fix

--- resources/js/Constants/EmbedFrame.jsx
+++ resources/js/Constants/EmbedFrame.jsx
@@ -17,15 +17,15 @@ export default function EmbedFrame({pText, pClassName, iframeClassName, url}) {
         <>
             <p className={`${pClassName} ${shakingAnimation}`}>{pText}</p>
             <iframe
-                // onMouseEnter={handleShakeStart}
-                // onMouseLeave={handleShakeStart}
+                onMouseEnter={handleShakeStart}
+                onMouseLeave={handleShakeStart}
                 // onTouchStart={handleShakeStart}
                 // onTouchEnd={handleShakeStart}
                 className={`${iframeClassName} ${shakingAnimation}`}
                 src={url}
                 title="Lictic Embed"
                 // frameBorder="1"
-                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                // allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
                 loading="lazy"
                 allowFullScreen
             ></iframe>
